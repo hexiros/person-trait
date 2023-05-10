29bad334551f4aebf2432ca86d1723f50f37d167
ruelrule <ruel@revenus.com>
2023-05-10 22:47:48 +0800

    change root

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Hexiros\PersonTrait\Http\Controllers\PersonController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware('api')
+    ->prefix('api')
+    ->group(function () {
+        // Route::resource('/user/{user}/person', PersonController::class);
+        Route::get('user/{user}/person', [PersonController::class, 'index']);
+        Route::post('user/{user}/person', [PersonController::class, 'store']);
+        Route::put('user/{user}/person', [PersonController::class, 'store']);
+        Route::delete('user/{user}/person', [PersonController::class, 'destroy']);
+    });
