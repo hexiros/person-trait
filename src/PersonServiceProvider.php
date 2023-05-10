6eefd00892e6f4470db7e88d1ab2eec20bb0d9b8
Ruel Rule <ruel@hexiros.com>
2023-05-11 03:09:33 +0800

    moved back service provider

--- src/PersonServiceProvider.php
+++ src/PersonServiceProvider.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace Hexiros\PersonTrait\Providers;
+namespace Hexiros\PersonTrait;
 
+use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
 
 class PersonServiceProvider extends ServiceProvider
@@ -51,8 +52,24 @@ class PersonServiceProvider extends ServiceProvider
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../database/migrations' => database_path('migrations'),
+                __DIR__.'/database/migrations' => $this->getMigrationFileName('create_person_info_table.php'),
             ], 'person-trait-migrations');
         }
+    }
+
+    /**
+     * Returns existing migration file if found, else uses the current timestamp.
+     * Thanks to spatie/laravel-permission
+     */
+    protected function getMigrationFileName(string $migrationFileName): string
+    {
+        $timestamp = date('Y_m_d_His');
+
+        $filesystem = $this->app->make(Filesystem::class);
+
+        return Collection::make([$this->app->databasePath().DIRECTORY_SEPARATOR.'migrations'.DIRECTORY_SEPARATOR])
+            ->flatMap(fn ($path) => $filesystem->glob($path.'*_'.$migrationFileName))
+            ->push($this->app->databasePath()."/migrations/{$timestamp}_{$migrationFileName}")
+            ->first();
     }
 }
