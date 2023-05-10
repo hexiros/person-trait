9d3feb3af018f4fb5091a3eae548c7c980a89406
Ruel Rule <ruel@hexiros.com>
2023-05-11 02:07:44 +0800

    updated register provider

--- src/PersonServiceProvider.php
+++ src/PersonServiceProvider.php
@@ -4,7 +4,13 @@ namespace Hexiros\PersonTrait;
 
 use Illuminate\Support\ServiceProvider;
 
-class PersonServiceProvider extends ServiceProvider {
+class PersonServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
     public function boot()
     {
         // Register an observer to automatically add the PersonTrait to any models that use it
@@ -12,14 +18,38 @@ class PersonServiceProvider extends ServiceProvider {
             $model::observe(PersonTraitObserver::class);
         }
 
+        // Load migrations
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
     }
 
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/person.php',
+            'person'
+        );
+    }
+
+    /**
+     * Register the package's publishable resources.
+     *
+     * @return void
+     */
     public function bootForPublishing()
     {
         $this->registerPublishing();
     }
 
+    /**
+     * Register the package's publishable resources.
+     *
+     * @return void
+     */
     protected function registerPublishing()
     {
         if ($this->app->runningInConsole()) {
