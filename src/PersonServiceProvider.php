29a49ab72f745ebe067df40e3c342abc7ac44e38
Ruel Rule <ruel@hexiros.com>
2023-05-11 10:56:24 +0800

    updated service provider file

--- src/PersonServiceProvider.php
+++ src/PersonServiceProvider.php
@@ -1,36 +1,31 @@
 <?php
 
-namespace Hexiros\PersonTrait;
+namespace Hexiros\PersonTrait\Providers;
 
-use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
 
 class PersonServiceProvider extends ServiceProvider
 {
     /**
-     * Bootstrap any package services.
+     * Bootstrap the application services.
      *
      * @return void
      */
     public function boot()
     {
-        // Register an observer to automatically add the PersonTrait to any models that use it
-        foreach ($this->app->make('config')->get('person.models', []) as $model) {
-            $model::observe(PersonTraitObserver::class);
-        }
-
-        // Load migrations
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
+        $this->registerPublishing();
     }
 
     /**
-     * Register any application services.
+     * Register the application services.
      *
      * @return void
      */
     public function register()
     {
-        
+        //
     }
 
     /**
@@ -38,38 +33,10 @@ class PersonServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function bootForPublishing()
+    private function registerPublishing()
     {
-        $this->registerPublishing();
-    }
-
-    /**
-     * Register the package's publishable resources.
-     *
-     * @return void
-     */
-    protected function registerPublishing()
-    {
-        if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__.'/../database/migrations/create_person_info_table.php.stub' => $this->getMigrationFileName('create_person_info_table.php'),
-            ], 'person-trait-migrations');
-        }
-    }
-
-    /**
-     * Returns existing migration file if found, else uses the current timestamp.
-     * Thanks to spatie/laravel-permission
-     */
-    protected function getMigrationFileName(string $migrationFileName): string
-    {
-        $timestamp = date('Y_m_d_His');
-
-        $filesystem = $this->app->make(Filesystem::class);
-
-        return Collection::make([$this->app->databasePath().DIRECTORY_SEPARATOR.'migrations'.DIRECTORY_SEPARATOR])
-            ->flatMap(fn ($path) => $filesystem->glob($path.'*_'.$migrationFileName))
-            ->push($this->app->databasePath()."/migrations/{$timestamp}_{$migrationFileName}")
-            ->first();
+        $this->publishes([
+            __DIR__.'/../database/migrations/create_person_info_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_person_info_table.php'),
+        ], 'person-trait-migration');
     }
 }
