435805a82ebab177dc378b2416918059a384ae31
Ruel Rule <ruel@hexiros.com>
2023-05-11 11:43:01 +0800

    updated migration file

--- src/Providers/PersonServiceProvider.php
+++ src/Providers/PersonServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Hexiros\PersonTrait\Providers;
 
+use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
 
 class PersonServiceProvider extends ServiceProvider
@@ -36,7 +37,22 @@ class PersonServiceProvider extends ServiceProvider
     private function registerPublishing()
     {
         $this->publishes([
-            __DIR__.'/../database/migrations/create_person_info_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_person_info_table.php'),
+            __DIR__.'/../database/migrations/create_personal_information_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_personal_information_table.php'),
         ], 'person-trait-migration');
+    }
+
+    /**
+     * Returns existing migration file if found, else uses the current timestamp.
+     */
+    protected function getMigrationFileName(string $migrationFileName): string
+    {
+        $timestamp = date('Y_m_d_His');
+
+        $filesystem = $this->app->make(Filesystem::class);
+
+        return Collection::make([$this->app->databasePath().DIRECTORY_SEPARATOR.'migrations'.DIRECTORY_SEPARATOR])
+            ->flatMap(fn ($path) => $filesystem->glob($path.'*_'.$migrationFileName))
+            ->push($this->app->databasePath()."/migrations/{$timestamp}_{$migrationFileName}")
+            ->first();
     }
 }
